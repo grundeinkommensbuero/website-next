9f98bb29a834a031e28ba04d883c536beda933c9
nmauersberg <n.mauersberg@gmail.com>
2022-01-24 12:38:09 +0100

    Configure image domain access

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['xbge-directus.frac.tools'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
