8ee9954a68eb6ddc1964dbef84d6b66c978d8dde
nmauersberg <n.mauersberg@gmail.com>
2022-01-25 17:52:36 +0100

    Custom colors and sizes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,44 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      violet: '#7d69f6',
+      aqua: '#46b4b4',
+      red: '#ff4664',
+    }),
+    textColor: (theme) => ({
+      ...theme('colors'),
+      violet: '#7d69f6',
+      aqua: '#46b4b4',
+      red: '#ff4664',
+    }),
+    borderColor: (theme) => ({
+      ...theme('colors'),
+      violet: '#7d69f6',
+      aqua: '#46b4b4',
+      red: '#ff4664',
+    }),
+    extend: {
+      spacing: {
+        '5%': '5%',
+        '10%': '10%',
+        '20%': '20%',
+        '50%': '50%',
+      },
+    },
   },
   plugins: [],
 };
