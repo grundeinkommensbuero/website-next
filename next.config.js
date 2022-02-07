868c47e6fc60c5ba74e9e1b4149c9f747f0e6771
nmauersberg <n.mauersberg@gmail.com>
2022-02-07 20:05:31 +0100

    Add locale to html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['de'],
+    defaultLocale: 'de',
+  },
   images: {
     domains: ['xbge-directus.frac.tools'],
   },
