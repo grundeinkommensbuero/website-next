adfa77a7264227d337066c23bf9512cebf4f3e7e
nmauersberg <n.mauersberg@gmail.com>
2022-03-16 00:10:13 +0100

    Login placeholder

---
+++ pages/einloggen.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const Login = (): ReactElement => {
+  return (
+    <div className="pageWidth">
+      <h1 className="my-16">login goes here :)</h1>
+    </div>
+  );
+};
+
+export default Login;
