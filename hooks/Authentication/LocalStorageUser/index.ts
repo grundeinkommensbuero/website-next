7dca9b0cc041c1d428e412959e2c64f6dd7d7d21
nmauersberg <n.mauersberg@gmail.com>
2022-03-15 23:51:39 +0100

    Amplify config

--- hooks/Authentication/LocalStorageUser/index.ts
+++ hooks/Authentication/LocalStorageUser/index.ts
@@ -7,9 +7,7 @@ export const useLocalStorageUser = (): [
   setUserId: ((userId: string) => void) | null
 ] => {
   // Reactive state for user id
-  const [userId, setUserIdState] = useState<string | null>(null);
-
-  const getLocalStorageUserId = () => {
+  const [userId, setUserIdState] = useState(() => {
     // Initial value comes from localStorage
     try {
       if (typeof window !== 'undefined') {
@@ -22,18 +20,17 @@ export const useLocalStorageUser = (): [
       console.warn('Error querying localStorage for user-id', err);
       return null;
     }
-  };
-
-  const localStorageUserId = getLocalStorageUserId();
-  setUserIdState(localStorageUserId);
+  });
 
   // Setter function updates state and localStorage
-  const setUserId = (userId: string): void => {
+  const setUserId = (userId: string) => {
     if (userId) {
       window.localStorage.setItem(USER_ID_KEY, userId);
     } else {
       window.localStorage.removeItem(USER_ID_KEY);
     }
+
+    setUserIdState(userId);
   };
 
   return [userId, setUserId];
