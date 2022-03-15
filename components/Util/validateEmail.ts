376659715ca813cd1b708168351bce9458e218e8
nmauersberg <n.mauersberg@gmail.com>
2022-03-16 00:39:53 +0100

    Add validateEmail util

---
+++ components/Util/validateEmail.ts
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+export const validateEmail = (email: string) => {
+  // eslint-disable-next-line no-useless-escape
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
