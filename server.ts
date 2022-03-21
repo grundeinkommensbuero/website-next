719832080ff77bc126d5067e65acfa1c49d08fea
nmauersberg <n.mauersberg@gmail.com>
2022-03-21 21:34:59 +0100

    Add express server to force ssl redirect

---
+++ server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import sslRedirect from 'heroku-ssl-redirect';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+    server.use(sslRedirect());
+    server.all('*', (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
