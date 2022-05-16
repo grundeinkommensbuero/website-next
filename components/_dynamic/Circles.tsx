da5cc810453003737b77a48e18e961cc517a393d
nmauersberg <n.mauersberg@gmail.com>
2022-05-16 14:24:01 +0200

    Customize language and color

--- components/_dynamic/Circles.tsx
+++ components/_dynamic/Circles.tsx
@@ -1,5 +1,5 @@
 import CirclesPink from '@circles-pink/web-client';
 
-const Circles = () => <CirclesPink />;
+const Circles = () => <CirclesPink lang="de" baseColor="#7d69f6" />;
 
 export default Circles;
