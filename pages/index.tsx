284fe81b2cae19e74a5dfc474f281fdeea62d6f8
nmauersberg <n.mauersberg@gmail.com>
2022-06-27 16:41:45 +0200

    Remove old hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,25 +10,6 @@ import { Hero } from '../components/Hero';
 const Start = ({ page }: PageProps): ReactElement => {
   return (
     <div className={s.container}>
-      <div className="flex mt-16 relative">
-        <div className="w-half">
-          <Image
-            priority={true}
-            src={`${process.env.NEXT_PUBLIC_DIRECTUS}assets/3e2ffd09-a09e-42ab-b234-19288361d727`}
-            alt="Logo der Expedition Grundeinkommen"
-            height={728}
-            width={1153}
-            layout="responsive"
-            className="z-10"
-          />
-        </div>
-        <div className="w-half pr-16 pl-6 pt-16">
-          <h2 className={`z-10 text-violet ${s.keyVisualClaim}`}>
-            <b>Hol das Grundeinkommen jetzt in deinen Wohnort!</b>
-          </h2>
-        </div>
-        <div className="bg-violet w-full h-10percent absolute bottom-0 z-0"></div>
-      </div>
       {page && (
         <>
           {page.hasHero && page.heroImage && (
