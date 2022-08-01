40f8e7fadf86f03854c387a80ca35da66ed2c5cd
Valioesi <vali_schagerl@web.de>
2022-08-01 17:31:02 +0200

    Link to profile as link, not action

--- layout/Header/MainMenu/UserMenuLink.tsx
+++ layout/Header/MainMenu/UserMenuLink.tsx
@@ -5,7 +5,6 @@ import { CustomMenuAction } from './CustomMenuAction';
 import { CustomEntry, CustomMenuLink } from './CustomMenuLink';
 import s from './style.module.scss';
 import { XbgeAppContext } from '../../../context/App';
-import { useRouter } from 'next/router';
 
 export const UserMenuLink = ({
   entry,
@@ -17,7 +16,6 @@ export const UserMenuLink = ({
   const { isAuthenticated, customUserData, userId, signUserOut } =
     useContext(AuthContext);
   const { togglePageBuilder } = useContext(XbgeAppContext);
-  const router = useRouter();
 
   if (!isAuthenticated) {
     return <CustomMenuLink entry={entry} currentRoute={currentRoute} />;
@@ -37,9 +35,11 @@ export const UserMenuLink = ({
           </div>
           <div className={s.dropdownContent}>
             <div className="my-4 mt-8">
-              <CustomMenuAction
+              <CustomMenuLink
+                currentRoute={currentRoute}
                 entry={{
-                  action: () => router.push(`/mensch/${userId}`),
+                  id: '1',
+                  slug: `/mensch/${userId}`,
                   label: 'Zum Profil',
                 }}
               />
