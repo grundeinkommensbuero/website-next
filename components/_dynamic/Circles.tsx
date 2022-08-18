c41a2402eca615ac2376ca91a78053d7b87a1d4c
nmauersberg <n.mauersberg@gmail.com>
2022-08-18 12:04:53 +0200

    Fix color

--- components/_dynamic/Circles.tsx
+++ components/_dynamic/Circles.tsx
@@ -3,18 +3,21 @@
 import CirclesPink from '@circles-pink/web-client';
 import { useContext } from 'react';
 import AuthContext from '../../context/Authentication';
+import { NoSsr } from '../Util/NoSsr';
 
 const Circles = () => {
   const { userId, isAuthenticated } = useContext(AuthContext);
   return (
     <>
       {isAuthenticated ? (
-        <CirclesPink
-          lang="de"
-          baseColor="#7d69f6"
-          email={`user-${userId}@xbge.de`}
-          voucherServerEnabled={false}
-        />
+        <NoSsr>
+          <CirclesPink
+            lang="de"
+            baseColor="#FB8298"
+            email={`user-${userId}@xbge.de`}
+            voucherServerEnabled={false}
+          />
+        </NoSsr>
       ) : (
         <p>Bitte melde dich an!</p>
       )}
