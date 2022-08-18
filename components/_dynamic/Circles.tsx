19d03a20a126f537c706f6e83f2c63ec391012a3
nmauersberg <n.mauersberg@gmail.com>
2022-08-19 00:34:54 +0200

    Update circles to 1.0.40

--- components/_dynamic/Circles.tsx
+++ components/_dynamic/Circles.tsx
@@ -5,25 +5,29 @@ import { useContext } from 'react';
 import AuthContext from '../../context/Authentication';
 import { NoSsr } from '../Util/NoSsr';
 
+const xbgeTheme = {
+  baseColor: '#FB8298',
+  cardColor: '#f9ccd4',
+  lightColor: '#FFEBEE',
+  darkColor: '#65655E',
+  textColorLight: '#fff',
+  textColorDark: '#7e7e7e',
+  bgColor: '#fff',
+};
+
 const Circles = () => {
   const { userId, isAuthenticated } = useContext(AuthContext);
-  return (
-    <>
-      {isAuthenticated ? (
-        <NoSsr>
-          {window && (
-            <CirclesPink
-              lang="de"
-              baseColor="#FB8298"
-              email={`user-${userId}@xbge.de`}
-              voucherServerEnabled={false}
-            />
-          )}
-        </NoSsr>
-      ) : (
-        <p>Bitte melde dich an!</p>
-      )}
-    </>
+  return isAuthenticated ? (
+    <NoSsr>
+      <CirclesPink
+        lang="de"
+        theme={xbgeTheme}
+        email={`user-${userId}@xbge.de`}
+        voucherServerEnabled={false}
+      />
+    </NoSsr>
+  ) : (
+    <p>Bitte melde dich an!</p>
   );
 };
 
