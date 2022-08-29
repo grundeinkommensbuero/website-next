e1ab643ba8ad004610bd3398cb51715626ab4a8f
Valioesi <vali_schagerl@web.de>
2022-08-29 19:31:31 +0200

    Text fixes

--- components/CirclesShare/ShareButtonRow.tsx
+++ components/CirclesShare/ShareButtonRow.tsx
@@ -65,7 +65,7 @@ Sobald mich 3 Menschen verifizieren, bekomme ich jeden Monat ein Grundeinkommen 
 Wenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :) Bitte verifiziere meinen Account, dann fängt mein Grundeinkommen an zu fließen. 
 
 Wenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen. Beides geht auf ${constructShareURL()}`;
-  const messengerText = `Hi!\n\nIch hab mir gerade einen Account bei Circles geholt. Das ist eine neue Grundeinkommens-Kryptowährung.\n\nSobald mich 3 Menschen verifizieren, bekomme ich jeden Monat Circles im Wert von 72 € – ein Leben lang.\n\nWenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :)\n\nWenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen.\n\nDanke dir herzlich!\n\n
+  const messengerText = `Hi!\n\nIch hab mir gerade einen Account bei Circles geholt. Das ist eine neue Grundeinkommens-Kryptowährung.\n\nSobald mich 3 Menschen verifizieren, bekomme ich jeden Monat Circles im Wert von 72 € – ein Leben lang.\n\nWenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :)\n\nWenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen.\n\nDanke dir herzlich!\n\n${constructShareURL()}
   `;
 
   return (
@@ -110,7 +110,7 @@ Wenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich 
 
         <TelegramShareButton
           title={messengerText}
-          url={constructShareURL()}
+          url={''}
           windowWidth={1200}
           windowHeight={1000}
           className={s.shareItem}
@@ -128,7 +128,7 @@ Wenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich 
 
         <WhatsappShareButton
           title={messengerText}
-          url={constructShareURL()}
+          url={''}
           windowWidth={1200}
           windowHeight={1000}
           className={s.shareItem}
