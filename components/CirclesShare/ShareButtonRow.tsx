81577b66f26436a2e1f89fd4c513a6ea7631aedf
Valioesi <vali_schagerl@web.de>
2022-08-31 18:44:48 +0200

    Circles share text change

--- components/CirclesShare/ShareButtonRow.tsx
+++ components/CirclesShare/ShareButtonRow.tsx
@@ -65,7 +65,7 @@ Sobald mich 3 Menschen verifizieren, bekomme ich jeden Monat ein Grundeinkommen 
 Wenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :) Bitte verifiziere meinen Account, dann fängt mein Grundeinkommen an zu fließen. 
 
 Wenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen. Beides geht auf ${constructShareURL()}`;
-  const messengerText = `Hi!\n\nIch hab mir gerade einen Account bei Circles geholt. Das ist eine neue Grundeinkommens-Kryptowährung.\n\nSobald mich 3 Menschen verifizieren, bekomme ich jeden Monat Circles im Wert von 72 € – ein Leben lang.\n\nWenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :)\n\nWenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen.\n\nDanke dir herzlich!\n`;
+  const messengerText = `Hi!\n\nIch hab mir gerade einen Account bei Circles geholt. Das ist eine neue Grundeinkommens-Kryptowährung.\n\nSobald mich 3 Menschen verifizieren, bekomme ich jeden Monat 720 Circles – ein Leben lang. Das entspricht heute einem Wert von 72 € pro Monat.\n\nWenn du schon einen Account bei Circles hast - könntest du meinen Account dann bitte verifizieren? Dann fängt mein Grundeinkommen an zu fließen :)\n\nWenn du noch keinen Account hast, kannst du dir natürlich auch einen für dich anlegen.\n\nDanke dir herzlich!\n`;
 
   return (
     <>
