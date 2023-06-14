b9b56febdd40e51ae16fa3b83c13df629b7de04b
Valioesi <vali_schagerl@web.de>
2023-06-14 12:39:55 +0200

    Deactivate Circles

--- components/_dynamic/Circles.tsx
+++ components/_dynamic/Circles.tsx
@@ -1,14 +1,11 @@
 // TS declaration file will follow soon
 // @ts-ignore
-import CirclesPink from '@circles-pink/web-client';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import AuthContext, { CirclesResumee } from '../../context/Authentication';
 import { useUpdateUser } from '../../hooks/Api/Users/Update';
 import { SmallSignup } from '../Forms/SmallSignup';
 import { NoSsr } from '../Util/NoSsr';
 import querystring from 'query-string';
-import translations from '../../data/transaltions.json';
-import CirclesSharingFeature from '../CirclesShare';
 
 const xbgeTheme = {
   baseColor: '#FB8298',
@@ -141,52 +138,7 @@ const Circles = () => {
               </>
             )}
             {customUserData && customUserData.email !== '' && (
-              <CirclesPink
-                lang="de" // app language
-                buyVoucherEurLimit={customUserData.circlesLimit || 35} // limit of vouchers that can be bought in eur
-                theme={xbgeTheme} // app color theme
-                xbgeCampaign={true} // enable xbge special components
-                xbgeSafeAddress={'0xB9AE1Ce83a6548f1395ddfC36673957B98Eb234D'}
-                strictMode={true} // only allow xbge linked safe address restore from localStorage
-                safeAddress={savedSafeAddress}
-                // ^ linked safeAddress for strict mode check
-                voucherShopEnabled={voucherShopEnabled} // enable voucher shop
-                onTrackingResumee={(
-                  updateResumee: (
-                    circlesResumee?: CirclesResumee
-                  ) => CirclesResumee
-                ) => {
-                  const circlesResumee = updateResumee(resumee);
-                  if (circlesResumee) {
-                    const safeAddress =
-                      circlesResumee.safeAddress ||
-                      resumee?.safeAddress ||
-                      null;
-                    const username =
-                      circlesResumee.username || resumee?.username || null;
-                    const mergedResumee = {
-                      ...circlesResumee,
-                      username,
-                      safeAddress,
-                    };
-                    saveCirclesTracking(mergedResumee);
-                  }
-                }} // get tracking resumee with app state
-                translations={translations} // json with app text
-                email={`user-${userId}@xbge.de`} // email to be send to circles garden
-                sharingFeature={
-                  customUserData?.store?.circlesResumee?.lastState?.tag !==
-                  'Dashboard' ? (
-                    <CirclesSharingFeature
-                      userData={customUserData}
-                      userId={userId}
-                    />
-                  ) : undefined
-                }
-                shadowFriends={
-                  customUserData.store?.referredByCirclesUsername || []
-                } // usernames of share link clicked users
-              />
+              <p>Das Projekt Circles ist nicht mehr vorhanden.</p>
             )}
           </>
         </NoSsr>
