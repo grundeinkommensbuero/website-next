dd84ca87cd58c35b701a3b3c0f3516a6ff45a4e3
joyponader <joyponader@gmail.com>
2023-09-24 00:20:13 +0200

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -11,7 +11,7 @@ import PageNotFound from './404';
 const IS_BERLIN_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Berlin";
 const IS_HAMBURG_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Hamburg";
 
-export const index_slug = IS_BERLIN_PROJECT ? 'start' : IS_HAMBURG_PROJECT ? 'start-hamburg' : 'start-hamburg', //set to start-hamburg on expedition-grundeinkommen.de also temporarily
+export const index_slug = IS_BERLIN_PROJECT ? 'start' : IS_HAMBURG_PROJECT ? 'start-hamburg' : 'start-hamburg'; //set to start-hamburg on expedition-grundeinkommen.de also temporarily
 
 export type PageProps = {
   page: Page | null;
