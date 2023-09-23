6a42d4d47b1f8ac69a93dece5cb659a407d125ad
joyponader <joyponader@gmail.com>
2023-09-23 16:33:32 +0200

    Update index.tsx
    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import s from './style.module.scss';
-import { PageProps } from './[id]';
-import { ReactElement } from 'react';
-import { Section } from '../components/Section';
-import { GetStaticProps } from 'next';
-import { getPageProps } from '../utils/getPageProps';
-import { Hero } from '../components/Hero';
+import s from "./style.module.scss";
+import { PageProps } from "./[id]";
+import { ReactElement } from "react";
+import { Section } from "../components/Section";
+import { GetStaticProps } from "next";
+import { getPageProps } from "../utils/getPageProps";
+import { Hero } from "../components/Hero";
 
-const IS_BERLIN_PROJECT = process.env.NEXT_PUBLIC_PROJECT === 'Berlin';
-const IS_HAMBURG_PROJECT = process.env.NEXT_PUBLIC_PROJECT === 'Hamburg';
+const IS_BERLIN_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Berlin";
+const IS_HAMBURG_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Hamburg";
 
 const Start = ({ page }: PageProps): ReactElement => {
   return (
@@ -34,11 +34,10 @@ const Start = ({ page }: PageProps): ReactElement => {
 
 export const getStaticProps: GetStaticProps = async ({ preview }) => {
   const pageProps = IS_BERLIN_PROJECT
-  ? await getPageProps('start', preview)
-  : IS_HAMBURG_PROJECT
-  ? await getPageProps('start-hamburg', preview)
-  : await getPageProps('start-hamburg', preview); //set to start-hamburg on expedition-grundeinkommen.de temporarily
-
+    ? await getPageProps("start", preview)
+    : IS_HAMBURG_PROJECT
+    ? await getPageProps("start-hamburg", preview)
+    : await getPageProps("start-hamburg", preview); //set to start-hamburg on expedition-grundeinkommen.de temporarily
 
   return {
     props: pageProps,
