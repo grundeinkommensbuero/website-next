57ec6100264bfaeeed1efac2ca0f1a5c12366f8f
joyponader <joyponader@gmail.com>
2023-09-23 17:01:30 +0200

    Update preview.ts

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,9 @@
 import { Directus } from '@directus/sdk';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const IS_BERLIN_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Berlin";
+const IS_HAMBURG_PROJECT = process.env.NEXT_PUBLIC_PROJECT === "Hamburg";
+
 const preview = async (req: NextApiRequest, res: NextApiResponse) => {
   // Check the token and next parameters
   // This token should only be known to this API route and the CMS
@@ -11,7 +14,12 @@ const preview = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
-  const slug = req.query.slug !== '/' ? req.query.slug : 'start-hamburg';
+  const slug = req.query.slug !== '/' ? req.query.slug 
+    : IS_BERLIN_PROJECT
+    ? 'start'
+    : IS_HAMBURG_PROJECT
+    ? 'start-hamburg'
+    : 'start-hamburg' //set to start-hamburg on expedition-grundeinkommen.de also temporarily
 
   // Fetch the headless CMS to check if the provided `slug` exists
   const directus = new Directus(process.env.DIRECTUS || '');
