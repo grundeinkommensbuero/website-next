ce68284f843909862201192d2f418f0382e6573f
joyponader <joyponader@gmail.com>
2023-12-19 23:19:49 +0100

    Update index.tsx
    Button: Mitmachen -> Spenden

--- layout/Header/MainMenu/Desktop/index.tsx
+++ layout/Header/MainMenu/Desktop/index.tsx
@@ -54,7 +54,7 @@ export const MainMenu = ({
         })}
         {IS_HAMBURG_PROJECT ? (
           <li>
-            <LinkButton href="/mitmachen-hamburg">Jetzt sammeln</LinkButton>
+            <LinkButton href="/#spenden">Jetzt spenden!</LinkButton>
           </li>
         ) : (
           <li>
