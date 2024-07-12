d33269af154daeeb02eac94778fcb3c266f3359c
joyponader <joyponader@gmail.com>
2024-07-13 00:22:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,25 +8,41 @@ import { Hero } from '../components/Hero';
 
 const Start = ({ page }: PageProps): ReactElement => {
   return (
-    <div className={s.container}>
-      {page && (
-        <>
-          {page.hasHero && (page.heroImage || page.heroHTML) && (
-            <Hero
-              heroHTML={page.heroHTML}
-              heroTitle={page.heroTitle}
-              heroSubTitle={page.heroSubTitle}
-              heroImage={page.heroImage}
-            />
-          )}
-          <section>
-            {page.sections.map((section: Section) => {
-              return <Section key={section.id} section={section} />;
-            })}
-          </section>
-        </>
+    <section>
+      {/* see https://legacy.reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml and https://blog.logrocket.com/using-dangerouslysetinnerhtml-react-application/ */}
+      {page.heroHTML && (
+        <div
+          className={s.heroHTMLContainer}
+          dangerouslySetInnerHTML={{ __html: page.heroHTML }}
+        />
       )}
-    </div>
+      {page.typeformId && (
+        <Widget
+          id={page.typeformId}
+          style={{ width: '100%', height: '500px' }}
+          className="my-form"
+        />
+      )}
+      {page.hasHero && page.heroImage && (
+        <Hero
+          heroTitle={page.heroTitle}
+          heroSubTitle={page.heroSubTitle}
+          heroImage={page.heroImage}
+        />
+      )}
+      <div className="pageWidth">
+        {page.title && <h2 className="my-8">{page.title}</h2>}
+      </div>
+      {page.sections.map((section: Section, index: number) => {
+        return (
+          <Section
+            key={section.id}
+            section={section}
+            isFirstSection={index === 0}
+          />
+        );
+      })}
+    </section>
   );
 };
 
