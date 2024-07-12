5c01723c332c61c4ddeff37e111d84530226d9a1
joyponader <joyponader@gmail.com>
2024-07-13 01:35:56 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Start = ({ page }: PageProps): ReactElement => {
       and 
       https://blog.logrocket.com/using-dangerouslysetinnerhtml-react-application/
       */}
-      {(page.heroHTML) && (
+      {page.heroHTML && (
         <div
           className={s.heroHTMLContainer}
           dangerouslySetInnerHTML={{ __html: page.heroHTML! }}
