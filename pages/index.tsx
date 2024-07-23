9422e4f30172be22b521cbde8da5b11401242913
joyponader <joyponader@gmail.com>
2024-07-24 00:33:22 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Start = ({ page }: PageProps): ReactElement => {
       {page.typeformId && (
         <Widget
           id={page.typeformId}
-          style={{ width: '100%', height: '500px' }}
+          style={{ width: '100%', height: '650px' }}
           className="my-form"
         />
       )}
