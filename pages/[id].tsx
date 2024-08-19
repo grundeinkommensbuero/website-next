ed9b7236f2ef77c02fa0c2c3f078d5642f6b7d1f
joyponader <joyponader@gmail.com>
2024-08-19 03:35:56 +0200

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -88,7 +88,7 @@ const PageWithSections = ({ page }: PageProps): ReactElement => {
             style={{ width: '100%', height: '650px' }}
             className="my-form"
           />
-          <p className="element">
+          <p className={cN([s.p], { [s.hamburg]: IS_HAMBURG_PROJECT })}>
             Das Formular wird nicht angezeigt? Dann klicke{' '}
             <a
               href={`https://expeditionbge.typeform.com/to/${page.typeformId}?utm_campaign=formular_error`}
