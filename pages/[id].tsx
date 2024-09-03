c9bd7ef6d9bd5a33e7d7de87cd34cc787af31370
joyponader <joyponader@gmail.com>
2024-09-03 23:49:19 +0200

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -48,7 +48,7 @@ window.addEventListener('message', (message) => {
   }
   if(message.data.message === 'sendLetterEntryHeightToParent'
   ) {
-    const heightPx = \`\${message.data.height}px\`
+    const heightPx = `${parseInt(message.data.height, 10) + 100}px`;
     document.getElementById('briefeintragung-iframe')?.setAttribute('height', heightPx)
   }
   if(message.data.message === 'sendLetterEntryRegistrationSuccessToParent'
